981aab66190d809dea6e121039a3d1c520b4ce4e
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-06-22 14:43:08 -0400

    Re-render with conda-smithy 1.0.3 to enable Python 3.x support.

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -41,7 +41,7 @@ conda clean --lock
 conda install --yes --quiet conda-forge-build-setup
 source run_conda_forge_build_setup
 
-# Embarking on 2 case(s).
+# Embarking on 6 case(s).
     set -x
     export CONDA_NPY=110
     export CONDA_PY=27
@@ -52,6 +52,34 @@ source run_conda_forge_build_setup
     set -x
     export CONDA_NPY=111
     export CONDA_PY=27
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=110
+    export CONDA_PY=34
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=111
+    export CONDA_PY=34
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=110
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_NPY=111
+    export CONDA_PY=35
     set +x
     conda build /recipe_root --quiet || exit 1
     /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root conda-forge --channel=main || exit 1
